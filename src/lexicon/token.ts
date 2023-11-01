2852050abbad4a9f5891e915701be84333e7176e
Mesquita <mesquita@aospa.co>
2023-11-01 08:43:18 -0300

    Token: Allow type to be null
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/lexicon/token.ts
+++ src/lexicon/token.ts
@@ -3,7 +3,7 @@ import {z} from "zod";
 export const Token = z.object({
     class: z.string(),
     lex: z.string(),
-    type: z.string(),
+    type: z.string().nullable(),
 })
 
 export type Token = z.infer<typeof Token>
