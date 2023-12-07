e1de02c75614cf735f259d7caff7916745f39e1a
Mesquita <mesquita@aospa.co>
2023-12-07 18:07:07 -0300

    syntax: split get token into a respective function
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/syntax/syntax.ts
+++ src/syntax/syntax.ts
@@ -1,13 +1,20 @@
 import {lexicon} from "../lexicon/lexicon";
 import {Token} from "../lexicon/token";
+import {PDA} from "../pda/pda";
+
+const lexiconGenerator = lexicon();
+let lexiconObject = lexiconGenerator.next();
+
+function getToken(): Token {
+    const token: Token = lexiconObject.value
+    lexiconObject = lexiconGenerator.next();
+    return token;
+}
 
 export function syntax() {
-    const lexiconGenerator = lexicon();
-    let lexiconObject = lexiconGenerator.next();
+    const pda = new PDA();
 
     while (!lexiconObject.done) {
-        const token: Token = lexiconObject.value
-        console.log(token);
-        lexiconObject = lexiconGenerator.next();
+        console.log(getToken());
     }
 }
