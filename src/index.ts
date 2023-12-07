a842f416df3c115ee3b6855a9a1846d8f526528b
Mesquita <mesquita@aospa.co>
2023-12-07 17:29:15 -0300

    index: don't print the symbol stable
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import {loadKeywordSymbols, printSymbolsTable} from "./lexicon/symbols";
+import {loadKeywordSymbols} from "./lexicon/symbols";
 import {syntax} from "./syntax/syntax";
 
 function main() {
     loadKeywordSymbols();
     syntax();
-    printSymbolsTable();
 }
 
 main();
