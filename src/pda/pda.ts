8d5fc863f726164b3063e15040521e1ea47d80df
Mesquita <mesquita@aospa.co>
2023-12-07 18:07:29 -0300

    pda: grammar: import grammar rules
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/pda/pda.ts
+++ src/pda/pda.ts
@@ -1,11 +1,15 @@
+import {grammar} from "./grammar";
+
 export class PDA {
     private TRANSITION_TABLE_FILE_PATH = 'src/pda/states/ShiftReduce-Table.csv';
     private csvConverter = require('convert-csv-to-json');
 
-    private readonly transitionTable: JSON;
+    private readonly transitionTable;
+    private readonly grammar;
 
     constructor() {
         this.transitionTable = this.csvConverter.fieldDelimiter(',')
             .getJsonFromCsv(this.TRANSITION_TABLE_FILE_PATH);
+        this.grammar = grammar;
     }
 }
