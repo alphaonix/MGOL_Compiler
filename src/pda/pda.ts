d64e567bcf534fcb0715efbd90a0652ebba2b1db
Mesquita <mesquita@aospa.co>
2023-12-08 09:37:36 -0300

    pda: struct it
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/pda/pda.ts
+++ src/pda/pda.ts
@@ -1,15 +1,41 @@
 import {grammar} from "./grammar";
+import {Token} from "../lexicon/token";
 
 export class PDA {
-    private TRANSITION_TABLE_FILE_PATH = 'src/pda/states/ShiftReduce-Table.csv';
-    private csvConverter = require('convert-csv-to-json');
-
+    private readonly TRANSITION_TABLE_FILE_PATH = 'src/pda/states/ShiftReduce-Table.csv';
+    private readonly csvConverter = require('convert-csv-to-json');
     private readonly transitionTable;
     private readonly grammar;
 
-    constructor() {
-        this.transitionTable = this.csvConverter.fieldDelimiter(',')
-            .getJsonFromCsv(this.TRANSITION_TABLE_FILE_PATH);
+    private stack: string[];
+    private isDone: boolean | undefined;
+    private lexiconObject;
+    private lexiconGenerator: Generator;
+
+    constructor(lexiconGenerator: Generator) {
+        this.transitionTable = this.csvConverter.fieldDelimiter(',').getJsonFromCsv(this.TRANSITION_TABLE_FILE_PATH);
         this.grammar = grammar;
+
+        this.lexiconGenerator = lexiconGenerator;
+        this.lexiconObject = lexiconGenerator.next();
+        this.isDone = this.lexiconObject.done;
+        this.stack = [];
+    }
+
+    private getToken(): Token {
+        const token: Token = this.lexiconObject.value
+        this.lexiconObject = this.lexiconGenerator.next();
+        this.isDone = this.lexiconObject.done;
+        return token;
+    }
+
+    public parse(){
+        this.stack.push('0');
+        let token: Token;
+
+        while (!this.isDone) {
+            token = this.getToken();
+            console.log(token);
+        }
     }
 }
--- src/syntax/syntax.ts
+++ src/syntax/syntax.ts
@@ -1,20 +1,8 @@
 import {lexicon} from "../lexicon/lexicon";
-import {Token} from "../lexicon/token";
 import {PDA} from "../pda/pda";
 
-const lexiconGenerator = lexicon();
-let lexiconObject = lexiconGenerator.next();
-
-function getToken(): Token {
-    const token: Token = lexiconObject.value
-    lexiconObject = lexiconGenerator.next();
-    return token;
-}
-
 export function syntax() {
-    const pda = new PDA();
-
-    while (!lexiconObject.done) {
-        console.log(getToken());
-    }
+    const lexiconGenerator = lexicon();
+    const pda = new PDA(lexiconGenerator);
+    pda.parse();
 }
