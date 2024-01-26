f8173085af0abce0c24fe179a2a6aa00dba2dd8c
Mesquita <mesquita@aospa.co>
2024-01-26 09:38:14 -0300

    syntax: add an easy access to debug the symbolsTable
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/syntax/syntax.ts
+++ src/syntax/syntax.ts
@@ -1,8 +1,10 @@
 import {lexicon} from "../lexicon/lexicon";
 import {PDA} from "../pda/pda";
+import {printSymbolsTable} from "../lexicon/symbols";
 
 export function syntax() {
     const lexiconGenerator = lexicon();
     const pda = new PDA(lexiconGenerator);
     pda.parse();
+    //printSymbolsTable()
 }
